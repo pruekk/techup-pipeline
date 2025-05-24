1b35a4bfe93e4f3a5cfca0084947ae77341a3eb9
Thanapat Munmanothum <bankthanapat@gmail.com>
2025-05-24 11:39:03 +0700

    test1

--- app.mjs
+++ app.mjs
@@ -12,69 +12,127 @@ app.get("/", (req, res) => {
   res.send("Hello TechUp!");
 });
 
-app.get("/posts", (req, res) => {
-  try {
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 6;
-    const category = req.query.category || "";
-    const keyword = req.query.keyword || "";
+app.get("/posts/:id?", (req, res) => {
 
-    const safePage = Math.max(1, page);
-    const safeLimit = Math.max(1, Math.min(100, limit));
+  if(req.params.id) {
 
-    let filteredPosts = blogPosts;
-    if (category) {
-      filteredPosts = blogPosts.filter(
-        (post) => post.category.toLowerCase() === category.toLowerCase()
-      );
+    const id = parseInt(req.params.id);
+    const post = blogPosts.find((post) => post.id === id); 
+
+    if (!post) {
+      return res.status(404).json({ error: "Blog post not found" });
     }
 
-    if (keyword) {
-      filteredPosts = filteredPosts.filter(
-        (post) =>
-          post.title.toLowerCase().includes(keyword.toLowerCase()) ||
-          post.description.toLowerCase().includes(keyword.toLowerCase()) ||
-          post.content.toLowerCase().includes(keyword.toLowerCase()) ||
-          post.category.toLowerCase().includes(keyword.toLowerCase())
-      );
+    res.json(post);
+
+  } else {
+    try {
+      const page = Number(req.query.page) || 1;
+      const limit = Number(req.query.limit) || 6;
+      const category = req.query.category || "";
+      const keyword = req.query.keyword || "";
+
+      const safePage = Math.max(1, page);
+      const safeLimit = Math.max(1, Math.min(100, limit));
+
+      let filteredPosts = blogPosts;
+      if (category) {
+        filteredPosts = blogPosts.filter(
+          (post) => post.category.toLowerCase() === category.toLowerCase()
+        );
+      }
+
+      if (keyword) {
+        filteredPosts = filteredPosts.filter(
+          (post) =>
+            post.title.toLowerCase().includes(keyword.toLowerCase()) ||
+            post.description.toLowerCase().includes(keyword.toLowerCase()) ||
+            post.content.toLowerCase().includes(keyword.toLowerCase()) ||
+            post.category.toLowerCase().includes(keyword.toLowerCase())
+        );
+      }
+
+      const startIndex = (safePage - 1) * safeLimit;
+      const endIndex = startIndex + safeLimit;
+
+      const results = {
+        totalPosts: filteredPosts.length,
+        totalPages: Math.ceil(filteredPosts.length / safeLimit),
+        currentPage: safePage,
+        limit: safeLimit,
+        posts: filteredPosts.slice(startIndex, endIndex),
+      };
+
+      if (endIndex < filteredPosts.length) {
+        results.nextPage = safePage + 1;
+      }
+
+      if (startIndex > 0) {
+        results.previousPage = safePage - 1;
+      }
+
+      return res.json(results);
+    } catch (e) {
+      return res.json({
+        message: e.message,
+      });
     }
 
-    const startIndex = (safePage - 1) * safeLimit;
-    const endIndex = startIndex + safeLimit;
-
-    const results = {
-      totalPosts: filteredPosts.length,
-      totalPages: Math.ceil(filteredPosts.length / safeLimit),
-      currentPage: safePage,
-      limit: safeLimit,
-      posts: filteredPosts.slice(startIndex, endIndex),
-    };
-
-    if (endIndex < filteredPosts.length) {
-      results.nextPage = safePage + 1;
-    }
-
-    if (startIndex > 0) {
-      results.previousPage = safePage - 1;
-    }
-
-    return res.json(results);
-  } catch (e) {
-    return res.json({
-      message: e.message,
-    });
   }
 });
 
-app.get("/posts/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = blogPosts.find((post) => post.id === id);
+// app.get("/posts", (req, res) => {
+//   try {
+//     const page = Number(req.query.page) || 1;
+//     const limit = Number(req.query.limit) || 6;
+//     const category = req.query.category || "";
+//     const keyword = req.query.keyword || "";
 
-  if (!post) {
-    return res.status(404).json({ error: "Blog post not found" });
-  }
+//     const safePage = Math.max(1, page);
+//     const safeLimit = Math.max(1, Math.min(100, limit));
 
-  res.json(post);
-});
+//     let filteredPosts = blogPosts;
+//     if (category) {
+//       filteredPosts = blogPosts.filter(
+//         (post) => post.category.toLowerCase() === category.toLowerCase()
+//       );
+//     }
+
+//     if (keyword) {
+//       filteredPosts = filteredPosts.filter(
+//         (post) =>
+//           post.title.toLowerCase().includes(keyword.toLowerCase()) ||
+//           post.description.toLowerCase().includes(keyword.toLowerCase()) ||
+//           post.content.toLowerCase().includes(keyword.toLowerCase()) ||
+//           post.category.toLowerCase().includes(keyword.toLowerCase())
+//       );
+//     }
+
+//     const startIndex = (safePage - 1) * safeLimit;
+//     const endIndex = startIndex + safeLimit;
+
+//     const results = {
+//       totalPosts: filteredPosts.length,
+//       totalPages: Math.ceil(filteredPosts.length / safeLimit),
+//       currentPage: safePage,
+//       limit: safeLimit,
+//       posts: filteredPosts.slice(startIndex, endIndex),
+//     };
+
+//     if (endIndex < filteredPosts.length) {
+//       results.nextPage = safePage + 1;
+//     }
+
+//     if (startIndex > 0) {
+//       results.previousPage = safePage - 1;
+//     }
+
+//     return res.json(results);
+//   } catch (e) {
+//     return res.json({
+//       message: e.message,
+//     });
+//   }
+// });
 
 export default app;
